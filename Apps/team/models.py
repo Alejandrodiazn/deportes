40f3f9c12e09dc969172c010fc6f5fdd58f16798
Alejandro Diaz <idiaz16@alumnos.uaq.mx>
2020-06-12 01:52:50 -0500

    Cambios en los modelos

--- Apps/team/models.py
+++ Apps/team/models.py
@@ -2,31 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class Student(models.Model):
-    SOFT18='SOFT18'
-    LAT18='LAT18'
-    PLAN_CHOICES=[
-        (SOFT18,'SOF18'),
-        (LAT18,'LAT18'),
-    ]
-
-    PRIMERO = 1
-    SEGUNDO = 2
-    TERCERO = 3
-    SEMESTER_CHOICES=[
-        (PRIMERO, 'Primero'),
-        (SEGUNDO, 'Segundo'),
-        (TERCERO, 'Tercero'),
-    ]
-
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    group = models.IntegerField()
-    semester = models.IntegerField(choices=SEMESTER_CHOICES, default=PRIMERO)
-    plan = models.CharField(max_length=7, choices=PLAN_CHOICES)
-    liberado = models.BooleanField(default=False, blank=True)
-    date_enrollment = models.DateTimeField(auto_now_add=True, blank=True)
-
 class Team(models.Model):
     ATLETISMO='Atletismo'
     BASKETBALL='Basketball'
@@ -65,4 +40,30 @@ class Team(models.Model):
     max_students = models.IntegerField(null=True)
     schedule = models.CharField(max_length=50)
     place = models.CharField(max_length=25, choices=PLACES_CHOICES, default=CANCHA_PASTO)
+
+class Student(models.Model):
+    SOFT18='SOFT18'
+    LAT18='LAT18'
+    PLAN_CHOICES=[
+        (SOFT18,'SOF18'),
+        (LAT18,'LAT18'),
+    ]
+
+    PRIMERO = 1
+    SEGUNDO = 2
+    TERCERO = 3
+    SEMESTER_CHOICES=[
+        (PRIMERO, 'Primero'),
+        (SEGUNDO, 'Segundo'),
+        (TERCERO, 'Tercero'),
+    ]
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    expediente = models.IntegerField(unique=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    group = models.IntegerField()
+    semester = models.IntegerField(choices=SEMESTER_CHOICES, default=PRIMERO)
+    plan = models.CharField(max_length=7, choices=PLAN_CHOICES)
+    liberado = models.BooleanField(default=False, blank=True)
+    date_enrollment = models.DateTimeField(auto_now_add=True, blank=True)
 
